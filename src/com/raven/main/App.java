e84be6972dddef484e7fefccc8831edb5f7dd8b6
JensDev <jensluna89@gmail.com>
2024-06-28 16:58:05 -0500

    Creacion de App.class

---
+++ src/com/raven/main/App.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.raven.main;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author JENS07
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Crear instancia de la clase principal y ejecutar la aplicación
+        SwingUtilities.invokeLater(() -> {
+            App app = new App();
+            app.start();
+        });
+    }
+    private void start(){
+        // Crear y mostrar el frame principal
+        Main mainFrame = new Main();
+        mainFrame.setVisible(true);
+
+        // Iniciar conexión a la base de datos en segundo plano
+        //new DatabaseConnectionTask(mainFrame).execute();
+    }
+    
+}
