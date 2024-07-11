2374167a0e405b034101df746a48980c2d9ab58f
Shirley <karenshirley777@gmail.com>
2024-07-11 18:03:39 -0500

    login a com.raven.main

--- src/com/raven/main/Login1.java
+++ src/com/raven/main/Login1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.raven.icon;
+package com.raven.main;
 
 import java.awt.Image;
 import java.awt.event.WindowAdapter;
